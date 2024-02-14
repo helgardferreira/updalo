4dcf2d73022649d26b3b51157df2873d1cf54194
Helgard Ferreira <hrfer3@gmail.com>
2024-02-14 06:05:56 +0200

    feat: implement more general and unix-friendly approach to configPath detection

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,20 @@
 module Main where
 
-import System.Environment (getArgs)
+import System.Environment (getArgs, getEnv)
 import UpdateAlacrittyOpacity
+
+getAlacrittyConfigPath :: IO String
+getAlacrittyConfigPath = do
+  homeDir <- getEnv "HOME"
+  let configPath = homeDir ++ "/.config/alacritty/alacritty.toml"
+  return configPath
 
 main :: IO ()
 main = do
+  -- Surely there's a more elegant way to do this
+  configPath <- getEnv "HOME" >>= \homeDir -> return $ homeDir ++ "/.config/alacritty/alacritty.toml"
   -- Expecting the first argument to be the new opacity value
   args <- getArgs
   case args of
-    [newOpacity] -> updateFileContent "/Users/helgardferreira/.config/alacritty/alacritty.toml" newOpacity
+    [newOpacity] -> updateFileContent configPath newOpacity
     _anyOtherArgs -> putStrLn "Usage: updalo <newOpacityValue>"
